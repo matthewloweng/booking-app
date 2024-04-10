b7c88d0e6039da99cf56eb9bbea48c2a7ef16d6b
Matthew Ng <matthew.j.ng@gmail.com>
2024-04-10 13:22:53 -0700

    initial commit - setup mongoDB backend, creating schemas

---
+++ api/routes/hotels.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+//Create
+router.post("/", (req, res) => {
+    
+})
+//Update
+
+//Delete
+
+//Get
+
+//Get All
+
+export default router
