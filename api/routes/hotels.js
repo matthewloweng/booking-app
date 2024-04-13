f8e4c3f44817958c57df8bf4582f4da61cd6804f
Matthew Ng <matthew.j.ng@gmail.com>
2024-04-13 11:00:19 -0700

    finished mongoDB CRUD operations

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -14,22 +14,56 @@ router.post("/", async (req, res) => {
     } catch (err) {
         res.status(500).json(err)
     }
-})
+});
 //Update
 router.put("/:id", async (req, res) => {
     try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body});
+        const updatedHotel = await Hotel.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body},
+            {new:true}
+        );
 
         res.status(200).json(updatedHotel);
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 //Delete
+router.delete("/:id", async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(
+            req.params.id
+        );
+        res.status(200).json("Hotel has been deleted");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //Get
+router.get("/:id", async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(
+            req.params.id,
+        );
+        res.status(200).json(hotel);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //Get All
+router.get("/", async (req, res) => {
+    try {
+        const hotels = await Hotel.find();
+        
+        res.status(200).json(hotels);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 export default router
